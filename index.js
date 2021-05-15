8d1f7acfbc3a58b5f60a4b59ada82f2bc44c4f15
Shawn Cochran <shawn.cochran31@gmail.com>
2021-05-15 15:01:05 -0600

    issue-3, app initialization

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-//const genMD = require('./Develop/utils/generateMarkdown');
+const genMD = require('./Utilities/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -15,19 +15,138 @@ const questions = [
     'What is your email address?'
 ];
 
+const [ title, description, installation, usage, license, test, GitHub, email ] = questions;
+
 // TODO: Create a function to write README file
 function writeToFile(data) {
-    fs.writeFile('./README.md', data, err => {
-        if(err) {
-            console.log(err)
+    fs.writeFile('./README.md', data, error => {
+        if(error) {
+            console.log(error)
             return
         }
         console.log('Created File Successfully')
     })
-};
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: title,
+            validate: confirmVal => {
+                if (confirmVal) {
+                    return true
+                } else {
+                    console.log('Please try again!')
+                    return false
+                }
+            }
+        },
+        {
+            type:'input',
+            name: 'description',
+            message: description,
+            validate: confirmVal => {
+                if (confirmVal) {
+                    return true
+                } else {
+                    console.log('Please try again!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: installation,
+            validate: confirmVal => {
+                if (confirmVal) {
+                    return true
+                } else {
+                    console.log('Please try again!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: usage,
+            validate: confirmVal => {
+                if (confirmVal) {
+                    return true
+                } else {
+                    console.log('Please try again!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: license,
+            choices: ['Apache', 'CC0', 'MIT', 'GNU GPLv3', 'None'],
+            validate: confirmVal => {
+                if (confirmVal) {
+                    return true
+                } else {
+                    console.log('Please try again!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: test,
+            validate: confirmVal => {
+                if (confirmVal) {
+                    return true
+                } else {
+                    console.log('Please try again!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'GitHub',
+            message: GitHub,
+            validate: confirmVal => {
+                if (confirmVal) {
+                    return true
+                } else {
+                    console.log('Please try again!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: email,
+            validate: confirmVal => {
+                if (confirmVal) {
+                    return true
+                } else {
+                    console.log('Please try again!')
+                    return false
+                }
+            }
+        },
+    ])
+}
 
 // Function call to initialize app
-init();
+init()
+.then(readmeData => {
+    return genMD(readmeData);
+})
+.then(pageMD => {
+    return writeToFile(pageMD);
+})
+.catch(error => {
+    console.log(error)
+});
